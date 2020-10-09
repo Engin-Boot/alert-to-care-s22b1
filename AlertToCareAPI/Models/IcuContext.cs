bf5849b6b5655a1b136e26f2b2cc7842a92ce567
varnikagautam <varnikagautam@users.noreply.github.com>
2020-10-09 13:44:02 +0530

    Project Added

---
+++ AlertToCareAPI/Models/IcuContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlertToCareAPI.Models
+{
+    public class IcuContext : DbContext
+    {
+        public IcuContext(DbContextOptions<IcuContext> options) : base(options)
+        {
+        }
+        public DbSet<PatientData> PatientData { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PatientData>().ToTable("PatientData");
+            
+        }
+
+    }
+}
+
---
+++ AlertToCareAPI/Models/PatientData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlertToCareAPI.Models
+{
+    public class PatientData
+    {
+        [Key]
+        public string PatientID { get; set; }
+        public string Name { get; set; }
+        public string BedID { get; set; }
+        public bool Vitals { get; set; }
+    }
+}
