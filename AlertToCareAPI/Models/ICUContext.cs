ec7128639debe3121e7913d1ea310bed4ad79fef
varnikagautam <varnikagautam@users.noreply.github.com>
2020-10-12 19:31:40 +0530

    New Update

---
+++ AlertToCareAPI/Models/ICUContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AlertToCareAPI.Models;
+
+namespace AlertToCareAPI.Models
+{
+    public class ICUContext : DbContext
+    {
+        public ICUContext(DbContextOptions<ICUContext> options) : base(options)
+        {
+        }
+        public DbSet<PatientData> PatientData { get; set; }
+        public DbSet<AlertToCareAPI.Models.BedData> BedData { get; set; }
+    }
+}
