48d0d0feb5131a9bb1d6f6ca16e50aa7de857b62
Vinay Bhutange <35776978+VinayBhutange@users.noreply.github.com>
2020-10-14 08:56:28 +0530

    removed reshaper error

--- AlertToCareAPI/Controllers/BedDatasController.cs
+++ AlertToCareAPI/Controllers/BedDatasController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using AlertToCareAPI.Models;
-using System.Configuration;
 
 namespace AlertToCareAPI.Controllers
 {
